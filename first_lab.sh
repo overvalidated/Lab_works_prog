b97d9edc3d31d2706f97314aff40a8e647dfa565
overvalidated <germaniga4@gmail.com>
2020-09-14 13:26:36 +0300

    Add files via upload

---
+++ first_lab.sh
@@ -0,0 +1,33 @@
+#!/bin/sh
+mkdir lab0/
+cd lab0
+touch bibarel
+touch drifblim1
+
+mkdir forretress1/
+cd forretress1
+touch sharpedo
+mkdir bronzong/
+mkdir servine/
+touch kadabra
+touch mantyke
+
+cd ../
+touch petilil7
+
+mkdir vileplume2/
+cd vileplume2
+mkdir scraggy
+touch altaria
+mkdir drowzee
+touch slugma
+mkdir alakazam
+
+cd ../
+
+mkdir weepinbell9/
+cd weepinbell9
+touch rapidash
+touch golduck
+mkdir rhyperior
+touch barboach
